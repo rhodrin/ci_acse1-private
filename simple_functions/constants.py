b6c9ddf3b18a79f23aca4587bd29bd626c6566cd
rhodrin <rhodri.nelson@gmail.com>
2020-10-16 14:47:32 +0100

    Update lru_cache calls for older versions of python.

--- simple_functions/constants.py
+++ simple_functions/constants.py
@@ -9,7 +9,7 @@ def pi(terms=1):
     return 1./(2.*sqrt(2.)/9801.*rsum(terms))
 
 
-@lru_cache  # Note: -> @cache in python >= 3.9
+@lru_cachelru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
 def rsum(n):
     t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
     return t + rsum(n-1) if n else t
