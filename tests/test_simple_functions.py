14e3a891740323139955df55da21e7ea652c2e0f
rhodrin <rhodri.nelson@gmail.com>
2020-10-19 18:02:27 +0100

    Add gauss.

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -1,6 +1,6 @@
+import numpy as np
 import pytest
-
-from simple_functions import my_sum, factorial
+from simple_functions import my_sum, factorial, gauss
 
 
 class TestSimpleFunctions(object):
@@ -24,3 +24,19 @@ class TestSimpleFunctions(object):
         '''Test our factorial function'''
         answer = factorial(number)
         assert answer == expected
+
+
+class TestGauss(object):
+    """ Class for testing the Gaussian elimination algorithm """
+    def test_one(self):
+        a = [[2, 9, 4], [7, 5, 3], [6, 1, 8]]
+        b = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        det, c = gauss(a, b)
+
+        assert det == -360.0
+        assert np.allclose(c, [[-0.10277777777777776, 0.18888888888888888,
+                                -0.019444444444444438],
+                               [0.10555555555555554, 0.02222222222222223,
+                                -0.061111111111111116],
+                               [0.0638888888888889, -0.14444444444444446,
+                                0.14722222222222223]])
